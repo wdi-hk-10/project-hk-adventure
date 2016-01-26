e43c7b8bc4aea92fa8dc35690b6ad1ea3fdc4402
cmtaubman <carmen@cgkcorporation.com>
2016-01-26 22:47:43 +0800

    var scene additions

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@
 // FUNCTIONS
   var Scene = function(option) {
     this.imageOne = option.imageOne;
-    this.imageTne = option.imageTwo;
+    this.imageTwo = option.imageTwo;
     this.question = option.question;
     this.buttonOne = option.buttonOne;
     this.buttonTwo = option.buttonTwo;
@@ -14,16 +14,32 @@
 
 // DIFFERENT SCENES
 var foodScene = new Scene({
-  imageOne: "./images/14517.jpg",
-  imageTwo: "./images/14517.jpg",
-  question: "Time to Eat. Where will you go?",
+  imageOne: "./images/dimsum.jpg",
+  imageTwo: "./images/fancybar2.jpg",
+  question: "It's 12pm. Time to Eat. Where do you go?",
   buttonOne: "Dim Sum!!!",
-  buttonTwo: "Swanky Bar!!!"
+  buttonTwo: "Swanky Restaurant"
+});
+
+var carbOverloadScene = new Scene({
+  imageOne: "./images/junk.jpg",
+  imageTwo: "./images/footMassage.jpg",
+  question: "Now you're full on carbs. Your friend has let you know their Junk boat is picking up more passengers at Central Pier. Do you get on the junk  - or would you prefer to have a Foot massage?",
+  buttonOne: "Junk party!!!",
+  buttonTwo: "Foot Massage"
+});
+
+var foodPoisoningScene = new Scene({
+  imageOne: "./images/hkTaxi.jpg",
+  imageTwo: "./images/hkMtr.jpg",
+  question: "The swanky restaurant in Central gave you food poisoning and you feel ill immediately. You need to get to the hospital ASAP - how do you travel?",
+  buttonOne: "Hong Kong taxi",
+  buttonTwo: "MTR and then walk (run)"
 });
 
 var junkScene = new Scene({
   imageOne: "./images/HK1.jpg",
-  imageTwo: "./images/HK2.jpg",
+  imageTwo: "./images/fancybar.jpg",
   question: "Now you're full on carbs. Want to go on a Junk or Foot massage?",
   buttonOne: "Junk time!!!",
   buttonTwo: "Foot Massage!!!"
