ab84c91f29ffaa1529f6afd91b22c7bb7be320b7
cmtaubman <carmen@cgkcorporation.com>
2016-01-28 14:46:30 +0800

    audio files added

--- JS/scenes.js
+++ JS/scenes.js
@@ -8,6 +8,7 @@ var Scene = function(option) {
   this.option1 = null;
   this.option2 = null;
   this.audioOne = option.audioOne ? new buzz.sound(option.audioOne): null;
+  this.audioTwo = option.audioTwo ? new buzz.sound(option.audioTwo): null;
   this.end = option.end || false;
 };
 
@@ -18,7 +19,8 @@ var foodScene = new Scene({
   question: "It's 12pm. Time to Eat. Where do you go?",
   buttonOne: "Dim Sum!!!",
   buttonTwo: "Swanky Restaurant",
-  audioOne: "./images/chat.mp3"
+  audioOne: "./images/chat.mp3",
+  audioTwo: "./images/sax.mp3"
 });
 
 var carbOverloadScene = new Scene({
@@ -26,7 +28,9 @@ var carbOverloadScene = new Scene({
   imageTwo: "./images/footMassage.jpg",
   question: "Now you're full on carbs. Your friend has let you know their Junk boat is picking up more passengers at Central Pier. Do you get on the junk  - or would you prefer to have a Foot massage?",
   buttonOne: "Junk party!!!",
-  buttonTwo: "Foot Massage"
+  buttonTwo: "Foot Massage",
+  audioOne: "./images/boat.mp3",
+  audioTwo: "./images/foot.mp3"
 });
 
 var foodPoisonScene = new Scene({
@@ -34,7 +38,9 @@ var foodPoisonScene = new Scene({
   imageTwo: "./images/hkMtr.jpeg",
   question: "The swanky restaurant in Central gave you food poisoning and you feel ill immediately. You need to get to the hospital ASAP - how do you travel?",
   buttonOne: "Hong Kong taxi",
-  buttonTwo: "MTR and then walk (run)"
+  buttonTwo: "MTR and then walk (run)",
+  audioOne: "./images/taxi.mp3",
+  audioTwo: "./images/mtr.mp3"
 });
 
 var afterJunkScene = new Scene({
@@ -42,7 +48,9 @@ var afterJunkScene = new Scene({
   imageTwo: "./images/wanChai.jpg",
   question: "The junk dropped you off at central pier where you continued to drink until 10pm. You are now wasted. Where would you like to go?",
   buttonOne: "LKF woohoo!!!",
-  buttonTwo: "WanChai woohoo!!!"
+  buttonTwo: "WanChai woohoo!!!",
+  audioOne: "./images/lkf.mp3",
+  audioTwo: "./images/tram.mp3"
 });
 
 var afterFootScene = new Scene({
@@ -50,7 +58,9 @@ var afterFootScene = new Scene({
   imageTwo: "./images/wanChai.jpg",
   question: "After the foot massage, you decided to get a 3 hour body massage. You are now ready to party - where do you go?",
   buttonOne: "LKF woohoo!!!",
-  buttonTwo: "WanChai woohoo!!!"
+  buttonTwo: "WanChai woohoo!!!",
+  audioOne: "./images/lkf.mp3",
+  audioTwo: "./images/tram.mp3"
 });
 
 var hospitalScene = new Scene({
@@ -58,7 +68,9 @@ var hospitalScene = new Scene({
   imageTwo: "./images/macau.jpg",
   question: "You get to the hospital in time. Doc gives you 7 drugs for each symptom. You miraculously feel better and feel super lucky. Where do you go to gamble with friends?",
   buttonOne: "Happy Valley races",
-  buttonTwo: "Macau"
+  buttonTwo: "Macau",
+  audioOne: "./images/horse.mp3",
+  audioTwo: "./images/casino.mp3"
 });
 
 var lkfScene = new Scene({
@@ -66,7 +78,9 @@ var lkfScene = new Scene({
   imageTwo: "./images/dragoni.jpg",
   question: "You are now in LKF. Where do you drink?",
   buttonOne: "7 Eleven all the way!!!",
-  buttonTwo: "Dragon I!"
+  buttonTwo: "Dragon I!",
+  audioOne: "./images/gameOver.mp3",
+  audioTwo: "./images/usher.mp3"
 });
 
 var wanChaiScene = new Scene({
@@ -74,7 +88,9 @@ var wanChaiScene = new Scene({
   imageTwo: "./images/drink.jpg",
   question: "You are now in Wanchai. What do you want to do?",
   buttonOne: "Karaoke!!!",
-  buttonTwo: "Drink!!!"
+  buttonTwo: "Drink!!!",
+  audioOne: "./images/chat.mp3",
+  audioTwo: "./images/chat.mp3"
 });
 
 var happyValleyScene = new Scene({
@@ -82,7 +98,9 @@ var happyValleyScene = new Scene({
   imageTwo: "./images/crazyCrowd.jpg",
   question: "You have arrived at Happy Valley. Where do you go to watch the races?",
   buttonOne: "Corporate box",
-  buttonTwo: "With the crazy crowd"
+  buttonTwo: "With the crazy crowd",
+  audioOne: "./images/chat.mp3",
+  audioTwo: "./images/chat.mp3"
 });
 
 var crazyCrowdScene = new Scene({
@@ -90,7 +108,9 @@ var crazyCrowdScene = new Scene({
   imageTwo: "./images/batGirl.jpg",
   question: "You're in the crowd and it's fancy dress all the way. Who do you party with?",
   buttonOne: "Darth Vader",
-  buttonTwo: "Bat girl"
+  buttonTwo: "Bat girl",
+  audioOne: "./images/chat.mp3",
+  audioTwo: "./images/chat.mp3"
 });
 
 var macauScene = new Scene({
@@ -98,7 +118,9 @@ var macauScene = new Scene({
   imageTwo: "./images/helicopter.png",
   question: "Wow it's off to Macau huh? How will you travel with your friends?",
   buttonOne: "Ferry",
-  buttonTwo: "Helicopter VIP all the way"
+  buttonTwo: "Helicopter VIP all the way",
+  audioOne: "./images/chat.mp3",
+  audioTwo: "./images/chat.mp3"
 });
 
 var end1Scene = new Scene({
@@ -124,7 +146,8 @@ var end3Scene = new Scene({
   imageTwo: "http://i.giphy.com/TMu8bS0hQxCX6.gif",
   question: "You nailed the latest Justin Bieber song. Went home a hero - well done.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end4Scene = new Scene({
@@ -132,7 +155,8 @@ var end4Scene = new Scene({
   imageTwo: "http://i.giphy.com/phsUpW8VbXgRO.gif",
   question: "In a case of mistaken identiy,a HK triad gang storms the bar. You drunkenly fight them off, and they all go home crying to their mother.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end5Scene = new Scene({
@@ -140,7 +164,8 @@ var end5Scene = new Scene({
   imageTwo: "http://i.giphy.com/Achrz20r7S7Di.gif",
   question: "You have a boring night out with boring people in suits. You wish you were out with the crowd having fun. Bad move - go home and sulk.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end6Scene = new Scene({
@@ -148,7 +173,8 @@ var end6Scene = new Scene({
   imageTwo: "http://i.giphy.com/Vj9poc6x6zg8E.gif",
   question: "Turns out Vader can kick it. You have the best night of your life.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end7Scene = new Scene({
@@ -156,7 +182,8 @@ var end7Scene = new Scene({
   imageTwo: "http://i.giphy.com/599WsrtJIj2b6.gif",
   question: "Bat girl turns out to be one of the most notorious con- artists in Hong Kong. You lose your wallet, phone, watch, clothes and dignity.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end8Scene = new Scene({
@@ -164,7 +191,8 @@ var end8Scene = new Scene({
   imageTwo: "http://i.giphy.com/AYKv7lXcZSJig.gif",
   question: "Your friends can't believe you chose the ferry and ditch you for the helicopter. You go to buy a ferry ticket only to be told they're all sold out. It then starts to rain. Go home and sulk that you chose a bad option.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 var end9Scene = new Scene({
@@ -172,7 +200,8 @@ var end9Scene = new Scene({
   imageTwo: "http://i.giphy.com/l3nWmPFBffbkCd3Ow.gif",
   question: "You win at everything. Pay for a night at the penthouse suite at the Wynn and have the Best. Night. Ever.",
   buttonOne: "",
-  buttonTwo: ""
+  buttonTwo: "",
+  end: true
 });
 
 
